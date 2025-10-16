bee8d6d052582507e32d789b07b51f6225a1103c
Alexandrine Dubé <alexandrine.dube@hotmail.com>
2025-10-16 11:20:21 -0400

    weather-report et weather-analyzer complete, sans commentaires dans le code

--- weather-analyzer/src/weather_analyzer/main.py
+++ weather-analyzer/src/weather_analyzer/main.py
@@ -1,2 +1,89 @@
+
+import os
+import csv
+import time
+import threading
+import concurrent.futures as cf
+
+def timing(fonction):
+    def wrapper(*args, **kwargs):
+        lock = threading.Lock()
+        start = time.perf_counter()
+        result = fonction(*args, **kwargs)
+        exec_time = time.perf_counter() - start
+        with lock:
+            return result, exec_time
+    return wrapper
+
+
+def read_weather_data(filename):
+    try: 
+        with open(filename, 'r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                yield {
+                    "date": line["Date"],
+                    "station": line["Station"],
+                    "temperature": float(line["Temperature"]),
+                    "pressure": float(line["Pressure"]),
+                }
+    except IOError as e:
+        print(f"Error while reading {filename}: {e}")
+
+
+@timing
+def read_analyze(file):
+    data = read_weather_data(file)
+    stations = []
+    temp = []
+    pressure = []
+    nb_lecture = 0
+    for line in data:
+        nb_lecture += 1
+        temp.append(line["temperature"])
+        pressure.append(line["pressure"])
+        if line["station"] not in stations:
+            stations.append(line["station"])
+    return {
+        "avg_temp": sum(temp) / len(temp),
+        "max_temp": max(temp),
+        "min_temp": min(temp),
+        "avg_pressure": sum(pressure) / len(pressure),
+        "nb_lectures": nb_lecture,
+        "nb_unique_stations": len(stations),
+    }
+
+
+def process_files_concurrently(filenames):
+    executor = cf.ThreadPoolExecutor(max_workers=10)
+    results = {}
+    with cf.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = {executor.submit(read_analyze, file): file for file in filenames}
+        for future in cf.as_completed(futures):
+            file = futures[future]
+            stats, exec_time = future.result()
+            results[file] = {"stats": stats, "exec_time": exec_time}
+    return results
+
+
+def write_report(filenames):
+    data = process_files_concurrently(filenames)
+    print(" === Weather Analysis Report === \n")
+    print("--- Statistics by File ---")
+    for filename, filedata in data.items():
+        print(f"File: {filename}")
+        print(f"Processed in {filedata['exec_time']} seconds")
+        print(f"    Records: {filedata['stats']['nb_lectures']}")
+        print(f"    Avg Temperature: {filedata['stats']['avg_temp']:.2f}°C")
+        print(f"    Min Temperature: {filedata['stats']['min_temp']:.2f}°C")
+        print(f"    Max Temperature: {filedata['stats']['max_temp']:.2f}°C")
+        print(f"    Avg Pressure: {filedata['stats']['avg_pressure']:.2f} hPa")
+        print(f"    Unique Stations: {filedata['stats']['nb_unique_stations']}")
+        print()
+
+
 def main() -> None:
     print("Hello from weather-analyzer!")
+    folder = "../data/"
+    files = [os.path.join(folder, file) for file in os.listdir(folder) if file.endswith(".csv")]
+    write_report(files)
--- weather-report/src/main.rs
+++ weather-report/src/main.rs
@@ -1,6 +1,83 @@
+
+use rand::Rng;
+
+enum StationType {
+    MontrealWeather,
+    QuebecCold,
+    SherbrookeSunlight,
+    HotOrCold,
+    WeatherCanada,
+}
+
+impl StationType {
+    fn to_string(&self) -> String {
+        match self {
+            StationType::MontrealWeather => "MontrealWeather".to_string(),
+            StationType::QuebecCold => "QuebecCold".to_string(),
+            StationType::SherbrookeSunlight => "SherbrookeSunlight".to_string(),
+            StationType::HotOrCold => "HotOrCold".to_string(),
+            StationType::WeatherCanada => "WeatherCanada".to_string(),
+        }
+    }
+}
+
+struct WeatherRecord {
+    date: String,
+    station: StationType,
+    temperature: f32,
+    pressure: f32,
+}
+
+impl WeatherRecord {
+    fn to_csv_line(&self) -> String {
+        format!("{},{},{:.1},{:.1}",
+        self.date,
+        self.station.to_string(),
+        self.temperature,
+        self.pressure)
+    }
+}
+
+
+fn generate_random_date() -> String {
+    let mut rng = rand::rng();
+    let month = rng.random_range(1..=12);
+    let year = rng.random_range(2020..=2025);
+    
+    let max_day = match month {
+        2 => 28,
+        4 | 6 | 9 | 11 => 30,
+        _ => 31,
+    };
+    let day = rng.random_range(1..=max_day);
+    
+    format!("{:04}-{:02}-{:02}", year, month, day)
+}
+
+
+fn generate_weather_record() -> WeatherRecord {
+    let mut rng = rand::rng();
+    let date = generate_random_date();
+    let station_num = rng.random_range(1..=5);
+    let station = match station_num {
+        1 => StationType::MontrealWeather,
+        2 => StationType::QuebecCold,
+        3 => StationType::SherbrookeSunlight,
+        4 => StationType::HotOrCold,
+        _ => StationType::WeatherCanada,
+    };
+    let temp = rng.random_range(-10.0..=40.0);
+    let pressure = rng.random_range(980.0..=1050.0);
+    
+    WeatherRecord { date: date, station: station, temperature: temp, pressure: pressure }
+}
+
+
 fn main() {
+    let nb_lines = rand::rng().random_range(10..=20);
     println!("Date,Station,Temperature,Pressure");
-    println!("2025-10-16,StationA,20,1015");
-    println!("2025-10-16,StationB,22,1013");
-    println!("2025-10-16,StationC,19,1018");
+    for _ in 0..nb_lines {
+        let line = generate_weather_record();
+        println!("{}", line.to_csv_line());
+    }
 }
